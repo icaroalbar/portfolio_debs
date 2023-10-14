9b7a71b9859aee49f41a5af0075d46ae4188d2a5
Icaro Albar <icaro.albar@live.com>
2023-10-14 18:50:43 -0300

    revert

--- src/app/portfolio/contratacoes/data.ts
+++ src/app/portfolio/contratacoes/data.ts
@@ -5,38 +5,30 @@ export const data = [
       type: "Estudo Analítico",
       local: "Escola de Governo e Gestão",
       image: "nova_lei_licitacoes/Slide1.png",
-      link: "https://portfolio-debs.s3.us-east-2.amazonaws.com/contratacao_ies/Slide1.png"
+      link: "portfolio/contratacoes/nova_lei_licitacoes"
     },
-    // {
-    //   title: "Curso de Capacitação para nova lei de licitações e contratos",
-    //   description: "Curso de capacitação para implementação da nova lei de licitações e contratos no município de Niterói",
-    //   type: "Estudo Analítico",
-    //   local: "Escola de Governo e Gestão",
-    //   image: "nova_lei_licitacoes/Slide1.png",
-    //   link: "portfolio/contratacoes/nova_lei_licitacoes"
-    // },
-  //   {
-  //     title: "Instituição de ensino superior - Inexigibilidade",
-  //     description: "Contratação de instituição de ensino superior para capacitação dos servidores da prefeitura de Niterói em competências transversais, em consonância ao Plano de Capacitação do Município.",
-  //     type: "Estudo Analítico",
-  //     local: "Escola de Governo e Gestão",
-  //     image: "contratacao_ies/Slide1.png",
-  //     link: "portfolio/contratacoes/contratacao_ies"
-  //   },     
-  //   {
-  //   title: "Roadmap para contratação de curso - Inexigibilidade",
-  //   description: "Consultoria para Escritório de Gestão de Projetos de Niterói.",
-  //   type: "Estudo Analítico",
-  //   local: "Escola de Governo e Gestão",
-  //   image: "curso_caixa/Slide1.png",
-  //   link: "portfolio/contratacoes/curso_caixa"
-  // },
-  // {
-  //   title: "Contratação de consultoria para o 'Niterói Que Queremos'.",
-  //   description: "Estudo analítico para contratação de consultoria para continuidade do Programa Niterói que Queremos - Edição 10 anos.",
-  //   type: "Estudo Analítico",
-  //   local: "Núcleo de Orçamento Setorial e Contratos",
-  //   image: "consultoria_nqq/Slide1.png",
-  //   link: "portfolio/contratacoes/consultoria_nqq"
-  // }
+    {
+      title: "Instituição de ensino superior - Inexigibilidade",
+      description: "Contratação de instituição de ensino superior para capacitação dos servidores da prefeitura de Niterói em competências transversais, em consonância ao Plano de Capacitação do Município.",
+      type: "Estudo Analítico",
+      local: "Escola de Governo e Gestão",
+      image: "contratacao_ies/Slide1.png",
+      link: "portfolio/contratacoes/contratacao_ies"
+    },     
+    {
+    title: "Roadmap para contratação de curso - Inexigibilidade",
+    description: "Consultoria para Escritório de Gestão de Projetos de Niterói.",
+    type: "Estudo Analítico",
+    local: "Escola de Governo e Gestão",
+    image: "curso_caixa/Slide1.png",
+    link: "portfolio/contratacoes/curso_caixa"
+  },
+  {
+    title: "Contratação de consultoria para o 'Niterói Que Queremos'.",
+    description: "Estudo analítico para contratação de consultoria para continuidade do Programa Niterói que Queremos - Edição 10 anos.",
+    type: "Estudo Analítico",
+    local: "Núcleo de Orçamento Setorial e Contratos",
+    image: "consultoria_nqq/Slide1.png",
+    link: "portfolio/contratacoes/consultoria_nqq"
+  }
 ]
