b3685a9e16f2b37c2e21b1fc252200d7efdfb24d
Icaro Albar <icaro.albar@live.com>
2023-10-14 19:05:12 -0300

    wegwgwevc

--- src/app/portfolio/contratacoes/contratacao_ies/page.tsx
+++ src/app/portfolio/contratacoes/contratacao_ies/page.tsx
@@ -1,15 +1,15 @@
 import { ImagePage } from "@/components/ImagePage";
 
-export default function Contratacoes() {
+export default function Solicitacoes() {
   return (
     <ImagePage 
-      title="Instituição de ensino superior - Inexigibilidade"
-      description="Contratação de instituição de ensino superior para capacitação dos servidores da prefeitura de Niterói em competências transversais, em consonância ao Plano de Capacitação do Município."
-      type="Estudo Analítico"
-      local="Escola de Governo e Gestão"
-      folder="contratacao_ies"
-      SlideTotal={17}
-      back='contratacoes'
-    />
+        title="Instituição de ensino superior - Inexigibilidade"
+        description="Contratação de instituição de ensino superior para capacitação dos servidores da prefeitura de Niterói em competências transversais, em consonância ao Plano de Capacitação do Município."
+        type="Estudo Analítico"
+        local="Escola de Governo e Gestão"
+        folder="contratacao_ies"
+        SlideTotal={17}
+        back='contratacoes'
+      />
   )
 }
